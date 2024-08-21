1c03b2ee399bb1774c7f605adef33c221f32083b
AlexanderPlaceDev <alexander_dev@hotmail.com>
2024-08-20 22:27:21 -0700

    Asistente
    Se agregaron animaciones al asistente en el menu

--- Assets/Mis Cosas/Scripits/Controladores/Gameplay/Menu/scr_BotonesMenuJuego.cs
+++ Assets/Mis Cosas/Scripits/Controladores/Gameplay/Menu/scr_BotonesMenuJuego.cs
@@ -8,6 +8,7 @@ public class scr_BotonesMenuJuego : MonoBehaviour
 {
     [SerializeField] GameObject[] Botones;
     [SerializeField] Color Seleccionado;
+    [SerializeField] Animator Asistente;
 
     private void Update()
     {
@@ -19,6 +20,25 @@ public class scr_BotonesMenuJuego : MonoBehaviour
         {
             Debug.Log(Botones[int.Parse(Boton[0].ToString())].gameObject.name);
             Botones[int.Parse(Boton[0].ToString())].transform.GetChild(0).GetComponent<Image>().color = Seleccionado;
+            switch (Boton[0])
+            {
+                case '0':
+                    {
+                        Asistente.GetComponent<Animator>().Play("Habilidades");
+                        break;
+                    }
+
+                case '1':
+                    {
+                        Asistente.GetComponent<Animator>().Play("Inventario");
+                        break;
+                    }
+                case '2':
+                    {
+                        Asistente.GetComponent<Animator>().Play("Guia");
+                        break;
+                    }
+            }
 
         }
         else
