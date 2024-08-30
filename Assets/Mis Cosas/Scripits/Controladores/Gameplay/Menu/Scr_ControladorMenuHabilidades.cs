515c4f869805ee84fe373e0695df3021194f6717
AlexanderPlaceDev <alexander_dev@hotmail.com>
2024-08-30 02:07:08 -0700

    Habilidades
    Mejora en el menu de habilidades

--- Assets/Mis Cosas/Scripits/Controladores/Gameplay/Menu/Scr_ControladorMenuHabilidades.cs
+++ Assets/Mis Cosas/Scripits/Controladores/Gameplay/Menu/Scr_ControladorMenuHabilidades.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Scr_ControladorMenuHabilidades : MonoBehaviour
@@ -9,11 +12,17 @@ public class Scr_ControladorMenuHabilidades : MonoBehaviour
     [SerializeField] float moveSpeed;
     [SerializeField] float scaleFactor;
     [SerializeField] GameObject[] Botones;
+    [SerializeField] GameObject ObjetoHabilidadSeleccionada;
     public GameObject BotonActual;
-
     public string HabilidadActual;
+    [SerializeField] Scr_CreadorHabilidades[] Habilidades;
+    [SerializeField] TextMeshProUGUI Puntos;
+
+    string HabilidadSeleccionada;
+    bool YaSelecciono = false;
 
     private RectTransform arbolRectTransform;
+
 
     void Start()
     {
@@ -32,15 +41,78 @@ public class Scr_ControladorMenuHabilidades : MonoBehaviour
 
     void Update()
     {
+        Puntos.text = PlayerPrefs.GetInt("PuntosDeHabilidad", 0).ToString();
+        SeleccionarHabilidad();
+
+        if (HabilidadActual == null || HabilidadActual == "")
+        {
+            ActualizarArbol();
+        }
+        else
+        {
+            ActualizarHabilidad();
+        }
+
+
+
+    }
+
+    private void SeleccionarHabilidad()
+    {
+        Debug.Log(HabilidadSeleccionada + HabilidadActual + YaSelecciono);
+        // Añade una verificación para asegurarte de que no se vuelva a seleccionar de inmediato
+        if (HabilidadSeleccionada == null && HabilidadActual != "" && Input.GetKeyDown(KeyCode.Mouse0) && !YaSelecciono)
+        {
+            YaSelecciono = true;
+            HabilidadSeleccionada = HabilidadActual;
+            Debug.Log("Habilidad Seleccionada");
+            ObjetoHabilidadSeleccionada.SetActive(true);
+        }
+    }
+    public void RechazarHabilidad()
+    {
+        Debug.Log("Rechaza Habilidad");
+
+        // Desactiva la selección de habilidad y resetea la variable de control
+        YaSelecciono = false;
+
+        // Asegúrate de que la UI relacionada con la selección de habilidad se oculte
+        ObjetoHabilidadSeleccionada.SetActive(false);
+
+        // Asegúrate de que la habilidad seleccionada se deseleccione
+        HabilidadSeleccionada = null;
+    }
+    private void ActualizarHabilidad()
+    {
+        if (BotonActual != null)
+        {
+            ObjetoHabilidadSeleccionada.transform.GetChild(0).GetComponent<Image>().sprite = BotonActual.GetComponent<Image>().sprite;
+            ObjetoHabilidadSeleccionada.transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = BotonActual.transform.GetChild(0).GetComponent<Image>().sprite;
+            foreach (Scr_CreadorHabilidades Habilidad in Habilidades)
+            {
+                if (Habilidad.NombreBoton == BotonActual.name)
+                {
+                    ObjetoHabilidadSeleccionada.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text =Habilidad.Nombre;
+                    ObjetoHabilidadSeleccionada.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text =Habilidad.Descripcion;
+                    ObjetoHabilidadSeleccionada.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text ="Costo: "+Habilidad.Costo;
+
+                }
+            }
+        }
+    }
+
+    private void ActualizarArbol()
+    {
+        Debug.Log("Habilidad Seleccionada");
         // Cambiar tamaño del árbol con la rueda del mouse
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
         if (scrollInput != 0f)
         {
-            if (arbolRectTransform.localScale.x > 0f || scrollInput>0)
+            if (arbolRectTransform.localScale.x > 0f || scrollInput > 0)
             {
                 arbolRectTransform.localScale += Vector3.one * scrollInput * scaleFactor;
             }
-            
+
         }
 
         // Mover el árbol cuando se presiona la rueda del mouse
@@ -49,14 +121,6 @@ public class Scr_ControladorMenuHabilidades : MonoBehaviour
             float moveX = Input.GetAxis("Mouse X") * moveSpeed * Time.deltaTime;
             float moveY = Input.GetAxis("Mouse Y") * moveSpeed * Time.deltaTime;
             arbolRectTransform.anchoredPosition += new Vector2(moveX, moveY);
-        }
-
-        if (BotonActual != null)
-        {
-            if (PlayerPrefs.GetString("Habilidad:" + BotonActual.name, "No") == "No" && Input.GetMouseButtonDown(0))
-            {
-                PlayerPrefs.SetString("Habilidad:" + BotonActual.name, "Si");
-            }
         }
     }
 
@@ -89,4 +153,17 @@ public class Scr_ControladorMenuHabilidades : MonoBehaviour
         }
         BotonActual = null;
     }
+
+    public void ComprarHabilidad()
+    {
+        if (BotonActual != null)
+        {
+            if (PlayerPrefs.GetString("Habilidad:" + BotonActual.name, "No") == "No")
+            {
+                PlayerPrefs.SetString("Habilidad:" + BotonActual.name, "Si");
+            }
+        }
+    }
+
+
 }
---
+++ Assets/Mis Cosas/Scripits/Controladores/Gameplay/Objetos/Scriptable objects/Scr_CreadorHabilidades.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CrearObjeto", menuName = "Crear Objeto/Habilidad", order = 0)]
+public class Scr_CreadorHabilidades : ScriptableObject
+{
+    public string Nombre;
+    public string NombreBoton;
+    public string Descripcion;
+    public int Costo;
+}
