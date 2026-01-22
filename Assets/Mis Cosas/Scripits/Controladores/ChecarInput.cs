cb014917804a9450c8b30c7a81fc951291663028
vigaassassins <81651887+vigaassassins@users.noreply.github.com>
2026-01-21 18:03:50 -0600

    arreglao

--- Assets/Mis Cosas/Scripits/Controladores/ChecarInput.cs
+++ Assets/Mis Cosas/Scripits/Controladores/ChecarInput.cs
@@ -28,22 +28,6 @@ public class ChecarInput : MonoBehaviour
         //Debug.LogError("aaa");
     }
 
-    public void Chechar_controlador_Interfaz()
-    {
-        CammbiarAction_UI();
-        if (playerInput.currentControlScheme == "Control" || playerInput.currentControlScheme == "PlayController")
-        {
-            Debug.LogError("aaaaaes");
-            CammbiarAction_UI();
-        }
-    }
-    public void Cerrar_controlador_Interfaz()
-    {
-        if (playerInput.currentControlScheme == "Control" || playerInput.currentControlScheme == "PlayController")
-        {
-            CammbiarAction_Player();
-        }
-    }
     public void CammbiarAction_UI()
     {
         if (playerInput == null)
